c93439b1812e0a6922c3a6464da0dba48b589be3
radiel13 <radiel13@gmail.com>
2020-01-12 21:33:22 +0300

    ch 001_6.1. Классический поиск перебором. [#33489]

---
+++ trainee/chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHas5Then4() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+}
