5a3ec41085e1c6e89f4a26f1323cd6ef0f615104
radiel13 <radiel13@gmail.com>
2020-01-13 22:06:41 +0300

    ch 001_6.4.1. Слово заканчивается с ... [#156317]

--- trainee/chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
+++ trainee/chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -22,4 +22,20 @@ public class EndsWithTest {
         boolean result = EndsWith.endsWith(word, post);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whenStartWithPrefixThenTrue2() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith2(word, post);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse2() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith2(word, post);
+        assertThat(result, is(false));
+    }
 }
