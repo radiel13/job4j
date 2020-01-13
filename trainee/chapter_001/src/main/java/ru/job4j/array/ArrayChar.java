489f582742a6f28f40978eb80a7532f1a6904460
radiel13 <radiel13@gmail.com>
2020-01-13 21:56:09 +0300

    ch 001_6.4. Слово начинается с ... [#41585]

--- trainee/chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ trainee/chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,11 +4,11 @@ public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = false;
-        for(int i = 0; i < pref.length; i++){
-            if(word[i] != pref[i]){
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
                 result = false;
                 break;
-            }else{
+            } else {
                 result = true;
             }
         }
