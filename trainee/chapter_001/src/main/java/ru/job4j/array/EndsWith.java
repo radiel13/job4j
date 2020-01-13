30929a5e332d40848d02338876e5f09c8fa2c37b
radiel13 <radiel13@gmail.com>
2020-01-13 21:53:55 +0300

    ch 001_6.4.1. Слово заканчивается с ... [#156317]

---
+++ trainee/chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        for(int i = word.length-1, j = post.length-1; j != 0; i--, j--){
+            if(word[i] != post[j]){
+                result = false;
+                break;
+            }else{
+                result = true;
+
+            }
+        }
+        return result;
+    }
+}
