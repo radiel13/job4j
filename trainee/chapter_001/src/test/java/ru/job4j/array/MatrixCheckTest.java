c3014b923ae8a2235832aabf5d7255fa41f89277
radiel13 <radiel13@gmail.com>
2020-01-14 21:34:02 +0300

    ch_001_6.7.2. Моно столбец в матрице. [#214127]

---
+++ trainee/chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixCheckTest {
+
+    @Test
+    public void whenHasMonoHorizontal() {
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoHorizontal4() {
+        char[][] input = {
+                {' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X'},
+        };
+        boolean result = MatrixCheck.monoHorizontal(input, 3);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        boolean result = MatrixCheck.monoVertical(input, 2);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoVertical2() {
+        char[][] input = {
+                {' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', 'X'},
+                {' ', ' ', ' ', 'X'},
+        };
+        boolean result = MatrixCheck.monoVertical(input, 3);
+        assertThat(result, is(true));
+    }
+}
