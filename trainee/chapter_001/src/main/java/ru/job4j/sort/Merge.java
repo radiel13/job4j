70e36d6e4fa7f354d795147c857f54efd8a2c09e
radiel13 <radiel13@gmail.com>
2020-01-18 15:59:33 +0300

    ch_001_Объединить два массива [#124482]

--- trainee/chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ trainee/chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -6,6 +6,32 @@ public class Merge {
 
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int y = 0;
+        while (y < rsl.length) {
+            if(j >= right.length && i < left.length){
+                rsl[y]=left[i];
+                i++;
+                y++;
+                continue;
+            }else if(i >= left.length && j < right.length){
+                rsl[y]=right[j];
+                j++;
+                y++;
+                continue;
+            }
+
+            if (i < left.length && left[i] <= right[j]) {
+                rsl[y] = left[i];
+                i++;
+
+            } else if (i < left.length && left[i] >= right[j]) {
+                rsl[y] = right[j];
+                j++;
+            }
+            y++;
+        }
 
         return rsl;
     }
@@ -13,9 +39,29 @@ public class Merge {
     public static void main(String[] args) {
         Merge process = new Merge();
         int[] rsl = process.merge(
-                new int[] {1, 3, 5},
-                new int[] {2, 4}
+                new int[]{1, 3, 5},
+                new int[]{2, 4}
         );
         System.out.println(Arrays.toString(rsl));
     }
 }
+//rsl[y] = left[i] > right[j]?left[i]:right[i];
+//y++;
+/*
+
+            if(right.length == 0){
+                for(int k = 0; k < left.length; k++){
+                    rsl[y] = left[k];
+                    y++;
+                }
+                break;
+            }*/
+
+/*
+                if(left.length == 0){
+                    for(int k = 0; k < right.length; k++){
+                        rsl[y] = right[k];
+                        y++;
+                    }
+                    break;
+                }*/
