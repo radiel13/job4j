9d87bdf0983d7c282c6de5d534130412a8b3da78
radiel13 <radiel13@gmail.com>
2020-01-25 14:37:45 +0300

    ch_002_2.Наследование.1. Конструктор по умолчанию. [#173889]

---
+++ trainee/chapter_001/src/main/java/ru/job4j/tracker/Tiger.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Tiger extends Predator {
+    public Tiger() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Animal a = new Animal();
+    }
+}
