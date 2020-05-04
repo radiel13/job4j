ecf3f23c8404dd49cb4afa86edb37cc7a65e948a
radiel13 <radiel13@gmail.com>
2020-05-04 15:40:35 +0300

    2_4__9.2. Тесты вывода на консоль в StartUI [#33585]

--- trainee/chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
+++ trainee/chapter_002/src/test/java/ru/job4j/oop/tracker/StartUITest.java
@@ -2,10 +2,64 @@ package ru.job4j.oop.tracker;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+
+    @Test
+    public void whenPrtMenu() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        StubInput input = new StubInput(
+                new String[]{"0"}
+        );
+        StubAction action = new StubAction() {
+
+        };
+        new StartUI().init(input, new Tracker(), new UserAction[]{action});
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. Stub action")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+
+    /*@Test
+    public void initTest() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        UserAction action = actions[select];
+        StubInput input = new StubInput(
+                new String[]{"0"}
+        );
+        StubAction action = new StubAction() {
+
+        };
+        new StartUI().init(input, new Tracker(), new UserAction[]{action});
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. === Create a new Item ===")
+                .add("1. === List of Items ===")
+                .add("2. === Replace Item ===")
+                .add("3. === Delete Item ===")
+                .add("4. === Find Item by ID ===")
+                .add("5. === Find Item by Name ===")
+                .add("6. === Exit the Program ===")
+                .add("Select: ")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+*/
   /*  @Test
     public void whenAddItem() {
         String[] answers = {"Fix PC", "NASTYUSHKA", "INDYUSHKA"};
