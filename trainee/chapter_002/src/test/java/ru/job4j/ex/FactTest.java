c2df7724eec721398681ee10f8396f46bd097b66
radiel13 <radiel13@gmail.com>
2020-05-05 15:32:46 +0300

    5_0.3. Тестирование исключений с junit [#219366]

---
+++ trainee/chapter_002/src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+
+
+import org.junit.Test;
+
+
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void calc() {
+        Fact.calc(-1);
+    }
+
+}
