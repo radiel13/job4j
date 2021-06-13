21c8898dab8ca12859bd13b0b1f0ac2d901d4d6c
radiel <radiel13@gmail.com>
2021-06-13 18:33:39 +0300

    0. Map, HashMap [#212652]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("radiel13@gmail.com", "KPA");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
