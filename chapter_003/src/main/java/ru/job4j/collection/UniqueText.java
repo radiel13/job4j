b7b507cba25cdb8aaf26cbf48d046e1df24fa02e
radiel <radiel13@gmail.com>
2021-06-13 17:42:53 +0300

    2. Идентичные тексты [#212655]

--- chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
+++ chapter_003/src/main/java/ru/job4j/collection/UniqueText.java
@@ -1,4 +1,22 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+
+        check.addAll(Arrays.asList(origin));
+
+        for (String s : text) {
+            if (!check.contains(s)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
 }
