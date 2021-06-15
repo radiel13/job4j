f9782ef2d037073f258391a7520d085be14945b1
radiel <radiel13@gmail.com>
2021-06-15 23:28:10 +0300

    4. Сортировка номера [#221284]

--- chapter_003/src/main/java/ru/job4j/collection/LexSort.java
+++ chapter_003/src/main/java/ru/job4j/collection/LexSort.java
@@ -6,17 +6,13 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        for (int i = 0; i < left.length(); i++) {
-            left = left.substring(0, left.indexOf('.'));
-            right = right.substring(0, right.indexOf('.'));
 
-            if (Integer.parseInt(left) != Integer.parseInt(right)) {
-                return Integer.compare(Integer.parseInt(left), Integer.parseInt(right));
-            }
+        int l = Integer.parseInt(left.substring(0, left.indexOf('.')));
+        int r = Integer.parseInt(right.substring(0, right.indexOf('.')));
 
-
+        if (r != l) {
+            return Integer.compare(l, r);
         }
         return 0;
     }
-
 }
