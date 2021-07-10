fb9f9e7e2f8a3c97cb8de70e94a4611902631331
radiel <radiel13@gmail.com>
2021-07-10 16:28:54 +0300

    0. Stream API [#58166]

---
+++ trainee/chapter_003/src/main/java/ru/job4j/stream/StreamAPI.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamAPI {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = -5; i < 5; i++) {
+            numbers.add(i);
+        }
+        System.out.println(numbers);
+
+        List<Integer> newNumbers = numbers.stream().filter(integer -> integer >= 0).collect(Collectors.toList());
+        System.out.println(newNumbers);
+    }
+}
